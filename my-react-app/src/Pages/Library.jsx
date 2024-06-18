0f4de47b0f2d4cfce5d6336fd1e1248887e47bbb
LebogangMputle <lebozikalala@gmail.com>
2024-06-18 14:44:04 +0200

    "Updated text content in Library.jsx"

--- my-react-app/src/Pages/Library.jsx
+++ my-react-app/src/Pages/Library.jsx
@@ -3,12 +3,12 @@ export default function Library() {
         <div className="about-page-container">
             {/* <img src={bgImg} className="about-hero-image" /> */}
             <div className="about-page-content">
-                <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-                <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
-                <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+                <h1>All Soungs.</h1>
+                <p>Track Discription</p>
+                <p>Add Info.</p>
             </div>
             <div className="about-page-cta">
-                <h2>Your destination is waiting.<br />Your van is ready.</h2>
+                <h2>Artist.<br />Title.</h2>
             </div>
         </div>
     );
