3039d8a97751d0a5231c65d64543fff5fe93d79b
LebogangMputle <lebozikalala@gmail.com>
2024-06-19 09:08:09 +0200

    "Updated App.jsx to use BrowserRouter and Routes from react-router-dom, and moved index.css import. Removed index.css import from main.jsx."

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,28 +1,29 @@
 // import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter , Routes ,Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
 import Library from './Pages/Library';
 import Favourites from './Pages/Favourites';
 import Search from './Pages/Search';
+import './index.css'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
+          <Routes>
             <Route exact path="/" component={Home} />
             <Route path="/library" component={Library} />
             <Route path="/favourites" component={Favourites} />
             <Route path="/search" component={Search} />
-          </Switch>
+          </Routes>
         </div>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
